c87253bf70f6cd69505ff1e6d2d0361682103685
Jeremy Schoenberger <schoenbergerjer@gmail.com>
2022-01-17 12:59:16 -0500

    fixed cover image route

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
 <body class="bg-gray-200 font-sans leading-normal tracking-normal">
 
 <!--Header-->
-<div class="w-full m-0 p-0 bg-cover bg-bottom" style="background-image:url('cover.jpg'); height: 60vh; max-height:460px;">
+<div class="w-full m-0 p-0 bg-cover bg-bottom" style="background-image:url('images/cover.jpg'); height: 60vh; max-height:460px;">
     <div class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
         <!--Title-->
         <p class="text-white font-extrabold text-3xl md:text-5xl">
